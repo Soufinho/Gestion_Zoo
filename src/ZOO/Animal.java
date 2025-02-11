fc194ff7c208a07314d22db614d1a759b02e3e3f
OUHAIMMED_So <s.ouhaimmed@lprs.fr>
2025-02-11 17:19:43 +0100

    Ajouts des attributs, du constructeur et des setter et getter dans le Animal.java

--- src/ZOO/Animal.java
+++ src/ZOO/Animal.java
@@ -6,5 +6,36 @@ public class Animal {
     private String bruit;
     private String manger;
 
+    public Animal(String name, String style_marche, String bruit, String manger) {
+        this.name = name;
+        this.style_marche = style_marche;
+        this.bruit = bruit;
+        this.manger = manger;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getStyle_marche() {
+        return style_marche;
+    }
+    public String getBruit() {
+        return bruit;
+    }
+    public String getManger() {
+        return manger;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setStyle_marche(String style_marche) {
+        this.style_marche = style_marche;
+    }
+    public void setBruit(String bruit) {
+        this.bruit = bruit;
+    }
+    public void setManger(String manger) {
+        this.manger = manger;
+    }
 
 }
