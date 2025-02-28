d0003cdfd86267e374df81bfa3e43b1b668df24b
Soufiane <s.ouhaimmed@lprs.fr>
2025-02-28 22:22:25 +0100

    Utilisation de l'arraylist : Appli.java

--- src/Application/Appli.java
+++ src/Application/Appli.java
@@ -1,4 +1,30 @@
 package Application;
 
+import java.util.ArrayList;
+
 public class Appli {
+
+    public static void main(String[] args) {
+        ArrayList<String> lion = new ArrayList<>();
+        lion.add("Lion");
+        lion.add("10");
+        lion.add("marche majestueusement");
+        lion.add("ROOAAAARRR");
+        lion.add("Carnivore");
+
+        ArrayList<String> oiseau = new ArrayList<>();
+        oiseau.add("Oiseau");
+        oiseau.add("4");
+        oiseau.add("vol");
+        oiseau.add("cui cui cui");
+        oiseau.add("insectivore");
+
+        ArrayList<String> serpent = new ArrayList<>();
+        serpent.add("Serpent");
+        serpent.add("6");
+        serpent.add("rampe");
+        serpent.add("SSSSSSSSSSSSS");
+        serpent.add("carnivore");
+
+    }
 }
