a7282cb9f85cebda206053acf4d5ec116ccab2ce
Soufiane <s.ouhaimmed@lprs.fr>
2025-02-28 22:20:53 +0100

    Création de l'affichage des caractéristiques dea animaux dans: Lion.java Oiseau.java Serpent.java

--- src/ZOO/Lion.java
+++ src/ZOO/Lion.java
@@ -5,4 +5,12 @@ public class Lion extends Animal {
     public Lion(String name, String style_marche, String bruit, String manger) {
         super(name, style_marche, bruit, manger);
     }
+
+    public void afficherLion() {
+        System.out.println("Animal : " + getName());
+        System.out.println("Régime alimentaire : " + getManger());
+        System.out.println("Âge : " + getAge() + " ans");
+        System.out.println("Sexe : " + getBruit());
+        System.out.println("Mode de déplacement : " + getStyle_marche());
+    }
 }
--- src/ZOO/Oiseau.java
+++ src/ZOO/Oiseau.java
@@ -4,4 +4,12 @@ public class Oiseau extends Animal {
     public Oiseau(String name, String style_marche, String bruit, String manger) {
         super(name, style_marche, bruit, manger);
     }
+
+    public void afficherOiseau() {
+        System.out.println("Animal : " + getName());
+        System.out.println("Régime alimentaire : " + getManger());
+        System.out.println("Âge : " + getAge() + " ans");
+        System.out.println("Sexe : " + getBruit());
+        System.out.println("Mode de déplacement : " + getStyle_marche());
+    }
 }
